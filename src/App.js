2e2f9139e93389c15861c54509408de5a87dc889
jonbrand <jonbranaraujo18@gmail.com>
2022-07-27 16:34:23 -0300

    feat: add routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
 import './App.css';
 import { Header } from './components/Header/Header';
 import { Home } from './Pages/Home/Home';
+import { Detail } from './components/Detail/Detail';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={ <Home /> } />
+          <Route path="/detail" element={ <Detail /> } />
+        </Routes>
+      </Router>
     </div>
   );
 }
