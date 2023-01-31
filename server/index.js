a08354e0152dc24c1599a5ce9163870853ebd30b
jonbrand <jonbranaraujo18@gmail.com>
2023-01-31 16:53:49 -0300

    feat: add login route

--- server/index.js
+++ server/index.js
@@ -1,17 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const database = require('./src/services/database');
+const cors = require('cors');
 
 const app = express();
 
 const routes = require("./src/routes/movies.routes");
+const usersRoutes = require('./src/routes/users.routes');
 
+// Middlewares
 app.use(bodyParser.json());
+
+app.use(cors());
 
 app.use(express.json());
 
+// Routes
 app.use("/", routes);
+app.use("/user", usersRoutes);
 
 app.listen(3333, () => {
-  console.log("Server Farmando!🚂🚂🚂")
+  console.log("Server Farmando na porta 3333!🚂🚂🚂")
 });
